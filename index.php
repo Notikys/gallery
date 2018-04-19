ae4f1bbef8e81d489b896e33773f8e0b866f1f44
Dmytro <notikys@gmail.com>
2018-04-20 00:57:20 +0300

    Alpha version my site
    gallery

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Gallery</title>
+        <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+    </head>
+        <body>
+            <?php echo "Alpha version 0.1"?>
+            <p class="imglist" style="max-width: 1000px;">
+                <a href="https://source.unsplash.com/juHayWuaaoQ/1500x1000" data-fancybox="images">
+                    <img src="https://source.unsplash.com/juHayWuaaoQ/240x160" />
+                </a>
+
+                <a href="https://source.unsplash.com/eWFdaPRFjwE/1500x1000" data-fancybox="images">
+                    <img src="https://source.unsplash.com/eWFdaPRFjwE/240x160" />
+                </a>
+
+                <a href="https://source.unsplash.com/i2KibvLYjqk/1500x1000" data-fancybox="images">
+                    <img src="https://source.unsplash.com/i2KibvLYjqk/240x160" />
+                </a>
+
+                <a href="https://source.unsplash.com/RFgO9B_OR4g/1500x1000" data-fancybox="images">
+                    <img src="https://source.unsplash.com/RFgO9B_OR4g/240x160" />
+                </a>
+
+                <a href="https://source.unsplash.com/7bwQXzbF6KE/1500x1000" data-fancybox="images">
+                    <img src="https://source.unsplash.com/7bwQXzbF6KE/240x160" />
+                </a>
+
+                <a href="https://source.unsplash.com/NhU0nUR7920/1500x1000" data-fancybox="images">
+                    <img src="https://source.unsplash.com/NhU0nUR7920/240x160" />
+                </a>
+            </p>
+            <p>
+                Powered by Notikys &#169
+            </p>
+        </body>
+</html>

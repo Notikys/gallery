b7e099420e0d016c19b85007e5c57897dbf39c7a
Dmytro <notikys@gmail.com>
2018-05-03 23:53:20 +0300

    Aded masifes but not conected

---
+++ includes/massif.php
@@ -0,0 +1,56 @@
+<?php
+$images = array(
+    'url' => 'https://source.unsplash.com/juHayWuaaoQ/1500x1000',
+    'name' => 'First image',
+    'width' => '1500px',
+    'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/eWFdaPRFjwE/1500x1000',
+        'name' => 'Second image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+
+    array(
+        'url' => 'https://source.unsplash.com/i2KibvLYjqk/1500x1000',
+        'name' => 'Third image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/RFgO9B_OR4g/1500x1000"',
+        'name' => 'Forth image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/7bwQXzbF6KE/1500x1000',
+        'name' => 'Fifth image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/NhU0nUR7920/1500x1000',
+        'name' => 'Sixth image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/random/1500x1000',
+        'name' => 'Seventh image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/random/1500x1000',
+        'name' => 'Eight image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
+    array(
+        'url' => 'https://source.unsplash.com/random/1500x1000',
+        'name' => 'Ninth image',
+        'width' => '1500px',
+        'height' => '1000px',
+    );
